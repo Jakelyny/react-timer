60ec83d1acdc71af5cdfc1c7398349fc3283103a
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-09 13:41:57 -0300

    Making it more interactive

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from './Formulario.module.scss';
 
-class Formulario extends React.Component {
+class Formulario extends React.Component <{
+        setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>> 
+}>{
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [ ...tarefasAntigas, { ...this.state}]);
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor={style.novaTarefa}>
                         Adicione uma tarefa
@@ -14,6 +27,8 @@ class Formulario extends React.Component {
                         type="text" 
                         name="tarefa" 
                         id="tarefa" 
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value})}
                         placeholder="Qual será a tarefa?"
                         required
                     />
@@ -26,13 +41,15 @@ class Formulario extends React.Component {
                         type="time" 
                         step="1"
                         name="tempo" 
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
                         id="tempo" 
                         min="00:00:00"
                         max="02:00:00"
                         required
                     />
                 </div>
-                <Botao>
+                <Botao type="submit">
                     Adicionar
                 </Botao>
             </form>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,18 +1,8 @@
-import React from "react";
+import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    },{
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    },{
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    } ]
+function Lista({ tarefas }: { tarefas: ITarefa[]}) {
     return (
         <aside className={style.listaTarefas}>
             <h2>Tarefas do dia</h2>
